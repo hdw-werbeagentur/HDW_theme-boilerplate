fb61b69903a3710ce9cb4d7890fb8c189c6a0c92
Anthony Spross <anthony@talented-monkeys.com>
2020-04-22 19:47:21 +0200

    Adding  disable comments functionality (b3a31067c35c769ca1e8430c832e22c8ec4ef24b)

--- functions.php
+++ functions.php
@@ -8,3 +8,8 @@ if ((defined('WP_SMTP_HOST') && WP_SMTP_HOST != "localhost") && WP_SMTP_USERNAME
     // Enable SMTP mailing via external smtp server
 	include get_template_directory() . '/config/wordpress/mail-configuration.functions.php';
 }
+
+if ( defined('WP_COMMENTS') && WP_COMMENTS == false ){ 
+    // Disable wordpress comment to avoid spam
+	include get_template_directory() . '/config/wordpress/comments-configuration.functions.php';
+}
