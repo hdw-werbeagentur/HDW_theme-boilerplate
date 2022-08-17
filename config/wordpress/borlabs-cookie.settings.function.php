06e8c32052c56fed28e2f778d948329e25f7f239
Tobias Feld <t.feld@hdw1.de>
2022-08-17 18:19:08 +0200

    Create borlabs-cookie.settings.function.php

---
+++ config/wordpress/borlabs-cookie.settings.function.php
@@ -0,0 +1,20 @@
+<?php
+use BorlabsCookie\Cookie\Config;
+
+$pluginPath = 'borlabs-cookie/borlabs-cookie.php';
+
+if ( is_plugin_active( $pluginPath ) ) {
+
+    $borlabsCookie = new Config();
+    $borlabsCookieConfig = $borlabsCookie->getConfig();
+
+    if ( defined('WP_ENV') && WP_ENV == 'production' ){
+        $borlabsCookieConfig['testEnvironment'] = 0;
+    }
+    else{
+        $borlabsCookieConfig['testEnvironment'] = 1;
+    }
+
+    $borlabsCookie->saveConfig($borlabsCookieConfig);
+}
+
