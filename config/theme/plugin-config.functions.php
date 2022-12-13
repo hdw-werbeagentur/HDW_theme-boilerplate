a953f1f25aa86adecebdea424492a631ae7f9570
Sven Stuppi <57223830+sstuppi@users.noreply.github.com>
2022-12-13 10:22:35 +0100

    check if cf7 exist for new filter

--- config/theme/plugin-config.functions.php
+++ config/theme/plugin-config.functions.php
@@ -1,3 +1,5 @@
 <?php
 //Disable automatic p tags for Contact Form 7
-add_filter('wpcf7_autop_or_not', '__return_false');
+if(has_action('wpcf7_init')) {
+    add_filter('wpcf7_autop_or_not', '__return_false');
+}
